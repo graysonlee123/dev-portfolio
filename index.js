1ce980b2b8e001c4bd0a7e94848f27f5513d17ca
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-24 14:50:02 -0400

    adds middleware for static folder

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const path = require('path');
 
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname, '/index.html'));
+  res.sendfile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, function() {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
