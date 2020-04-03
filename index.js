7bc7012c2f7724af5429a0708738d52b26fb8b3b
graysonlee123 <graysonleegantek@gmail.com>
2020-04-03 19:31:42 -0400

    adds two more testimonials; using Swiper for testimonial slider; cleans up JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  grabCursor: true
+});
