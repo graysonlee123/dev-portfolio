a791f107ecba5af0cd11ceae324c0cea4bdebc9d
graysonlee123 <graysonleegantek@gmail.com>
2020-04-12 12:43:47 -0400

    disables scrolling for body when menu is open

--- index.js
+++ index.js
@@ -19,14 +19,30 @@ let isOpen = false;
 
 hamburger.onclick = function toggleMenu() {
   const menu = document.getElementById('mobile-menu');
+  const scrollY = window.scrollY;
   
   if (isOpen) {
     menu.classList.remove('active');
     hamburger.classList.remove('active');
+
+    // Unset Prevent Scrolling
+    const scrollY = document.body.style.top;
+    
+    document.body.style.position = '';
+    document.body.style.top = '';
+
+    //  Scroll to old position
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    
     isOpen = false;
   } else {
     menu.classList.add('active');
     hamburger.classList.add('active');
+    
+    // Prevent scrolling
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+    
     isOpen = true;
   }
 }
